28c925c19062a374a2cbc417def0a6efb29672ed
元谷 <lifulinghan@aol.com>
2018-08-30 16:53:23 +0800

    first commit

---
+++ build.py
@@ -0,0 +1,87 @@
+#-*-coding:utf-8-*-
+import os
+import sys
+import platform
+import smtplib
+import email.MIMEMultipart
+import email.MIMEText
+import email.MIMEBase
+
+
+#获取python文件所在的路径
+def p():
+    frozen = "not"
+    if getattr(sys, 'frozen',False):
+        frozen = "ever so"
+        return os.path.dirname(sys.executable)
+
+    return os.path.split(os.path.realpath(__file__))[0]
+
+pyPath = p()
+
+#更改工具目录
+os.chdir(p())
+#拉取代码
+os.system("git clone https://github.com/yuangu/sxtwl_cpp.git")
+#代码完成拉取
+os.chdir(os.path.join(pyPath, "./sxtwl_cpp/python"))
+
+if platform.system() == 'Windows':
+    os.system("python setup.py  bdist_wininst")
+
+print(u"=================打包完成========")
+#更换到打包好的目录里
+os.chdir(os.path.join(pyPath, "./sxtwl_cpp/python/dist"))
+
+file_name = ""
+dirs = os.listdir( "./" )
+
+for file in dirs:
+   if file[-3:] == "exe":
+        file_name = file
+      
+
+#帐号和密码来源https://github.com/normal-four/test/blob/815393a266142ba64df0402f9a6c15c203b95156/spider/mailtest.py
+
+username = "wzp_test@126.com"
+passwd = "a1269325139"
+
+
+server = smtplib.SMTP('smtp.126.com')
+server.login(username,passwd)
+main_msg = email.MIMEMultipart.MIMEMultipart()
+# 构造MIMEText对象做为邮件显示内容并附加到根容器
+text_msg = email.MIMEText.MIMEText("this is a test text to text mime")
+main_msg.attach(text_msg)
+ 
+# 构造MIMEBase对象做为文件附件内容并附加到根容器
+contype = 'application/octet-stream'
+maintype, subtype = contype.split('/', 1)
+ 
+## 读入文件内容并格式化
+data = open(file_name, 'rb')
+file_msg = email.MIMEBase.MIMEBase(maintype, subtype)
+file_msg.set_payload(data.read( ))
+data.close( )
+email.Encoders.encode_base64(file_msg)
+ 
+## 设置附件头
+basename = os.path.basename(file_name)
+file_msg.add_header('Content-Disposition',
+ 'attachment', filename = basename)
+main_msg.attach(file_msg)
+ 
+# 设置根容器属性
+main_msg['From'] = username
+main_msg['To'] = "1143402671@qq.com"
+main_msg['Subject'] = "[sxtwl]打包结果通知"
+main_msg['Date'] = email.Utils.formatdate( )
+ 
+# 得到格式化后的完整文本
+fullText = main_msg.as_string( )
+ 
+# 用smtp发送邮件
+try:
+    server.sendmail(main_msg['From'], main_msg['To'], fullText)
+finally:
+    server.quit()
