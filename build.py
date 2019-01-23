e351e3771e46fb14abadc8a3f2c3512c3eb46d1e
yuangu <lifulinghan@aol.com>
2019-01-23 13:49:09 +0800

    精简

--- build.py
+++ build.py
@@ -34,34 +34,37 @@ os.chdir(p())
 #拉取代码
 os.system("git clone https://github.com/yuangu/sxtwl_cpp.git")
 
-# print("========执行转码开始================")
-# for cxxPath in ["./sxtwl_cpp/src", "./sxtwl_cpp/python"]:
-#     src_path = os.path.join(pyPath, cxxPath)
-#     dirs = os.listdir( src_path  )
-#     for file in dirs:
-#         if file[-3:] == "cpp" or file[-1] == 'h' or file[-3:] == "cxx" :     
-#             filename = os.path.join(src_path, file)
-#             content = "".join(open(filename).readlines())
-#             try:
-#                 content = content.decode("utf8").encode("gbk") #如果是utf8编码就转成gbk
-#                 f = open(filename, "w")
-#                 f.write(content)
-#                 f.close()
-#                 print("转码完成:" + filename )
-#             except:
-#                 print("转码失败:" + filename)
-#                 continue
-
-# print("========执行转码完成================") 
-
 #代码完成拉取
 os.chdir(os.path.join(pyPath, "./sxtwl_cpp/python"))
 
 print("=================开始执行python setup.py ========")
 if platform.system() == 'Windows':
     print("打包执行中")
+    PythonList = (
+        "C:\\Python26\\python.exe",
+        "C:\\Python26-x64\\python.exe",
+        "C:\\Python27\\python.exe",
+        "C:\\Python27-x64\\python.exe",
+        "C:\\Python33\\python.exe",
+        "C:\\Python33-x64\\python.exe",
+        "C:\\Python34\\python.exe",
+        "C:\\Python34-x64\\python.exe",
+        "C:\\Python35\\python.exe",
+        "C:\\Python35-x64\\python.exe",
+        "C:\\Python36\\python.exe",
+        "C:\\Python36-x64\\python.exe",
+        "C:\\Python37\\python.exe",
+        "C:\\Python37-x64\\python.exe",
+    )
+
+
+    for python in PythonList:
+        os.system( python + "  --version")
+        os.system( python + "  -m pip install --upgrade pip")
+        os.system( python + "  -m pip install --upgrade setuptools")
+        os.system( python + "  -m pip  install wheel")
+        os.system( python + "  setup.py bdist_wheel")
     #print(os.system("python setup.py  bdist_wininst"))
-    print(os.system("python setup.py bdist_wheel"))
 print("=================打包完成========")
 
 
@@ -70,7 +73,6 @@ print("=================打包完成========")
 
 file_name = "dist.zip"
 Utils.makeZipFile(file_name, os.path.join(pyPath, "./sxtwl_cpp/python/dist"))
-
 
 
 #帐号和密码来源https://github.com/normal-four/test/blob/815393a266142ba64df0402f9a6c15c203b95156/spider/mailtest.py
@@ -82,8 +84,16 @@ passwd = "a1269325139"
 server = smtplib.SMTP('smtp.126.com')
 server.login(username,passwd)
 main_msg = MIMEMultipart.MIMEMultipart()
+
+
+from ffsend import upload
+url, owner =  upload("https://send.firefox.com/", file_name)
+file_name = None
+msg = u"这是一封自动发送的邮件，请不回复!\n您的打包结果下载地址:%s" %(url,)
+
+
 # 构造MIMEText对象做为邮件显示内容并附加到根容器
-text_msg = MIMEText.MIMEText("这是一封自动发送的邮件。请不要回复。",'plain', "utf-8")
+text_msg = MIMEText.MIMEText(msg,'plain', "utf-8")
 main_msg.attach(text_msg)
  
 # 构造MIMEBase对象做为文件附件内容并附加到根容器
@@ -91,17 +101,17 @@ contype = 'application/octet-stream'
 maintype, subtype = contype.split('/', 1)
  
 ## 读入文件内容并格式化
-data = open(file_name, 'rb')
-file_msg = MIMEBase.MIMEBase(maintype, subtype)
-file_msg.set_payload(data.read( ))
-data.close( )
-Encoders.encode_base64(file_msg)
+# data = open(file_name, 'rb')
+# file_msg = MIMEBase.MIMEBase(maintype, subtype)
+# file_msg.set_payload(data.read( ))
+# data.close( )
+# Encoders.encode_base64(file_msg)
  
-## 设置附件头
-basename = os.path.basename(file_name)
-file_msg.add_header('Content-Disposition',
- 'attachment', filename = basename)
-main_msg.attach(file_msg)
+# ## 设置附件头
+# basename = os.path.basename(file_name)
+# file_msg.add_header('Content-Disposition',
+#  'attachment', filename = basename)
+# main_msg.attach(file_msg)
  
 # 设置根容器属性
 main_msg['From'] = username
@@ -110,14 +120,14 @@ main_msg['To'] = "1143402671@qq.com"
 main_msg['Date'] = formatdate( )
  
 #带上python版本的信息
-ext = ""
-if "PYTHON_VERSION" in  os.environ:
-    ext += "Python_" + os.environ['PYTHON_VERSION']
+# ext = ""
+# if "PYTHON_VERSION" in  os.environ:
+#     ext += "Python_" + os.environ['PYTHON_VERSION']
 
-if "PYTHON_ARCH" in  os.environ:
-    ext += "_" + os.environ['PYTHON_ARCH']
+# if "PYTHON_ARCH" in  os.environ:
+#     ext += "_" + os.environ['PYTHON_ARCH']
 
-main_msg['Subject'] = "[sxtwl]打包结果通知" + '('+ ext + ')'
+main_msg['Subject'] = "[sxtwl]打包结果通知" 
 
 # 得到格式化后的完整文本
 fullText = main_msg.as_string( )
