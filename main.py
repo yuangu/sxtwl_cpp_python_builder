b709c5c330546a7391f1106e4889ea35e54bc849
yuangu <yuangu@huoyaojing.com>
2021-11-07 13:44:42 +0800

    测试

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import platform
 import os
+import sys
+
+sys.path.append("./build")
 
 ## 如果是python构建任务，执行python构建任务
 if os.getenv('BUILDFOR') == "python":
     from build import python
-    python.PythonBuild.run()
+    python.PythonBuild().run()
 else:
     print("不支持的构建语言目标")
+
