a5ae6be94defea81c1a4e6b748eb14bbcdb21b02
yuangu <yuangu@huoyaojing.com>
2021-11-07 16:56:24 +0800

    测试python环境

--- build/python.py
+++ build/python.py
@@ -3,9 +3,10 @@ import platform
 
 class PythonBuild(base.BuildBase):
     pythonList = []
+    twinePython = '' #用户twine提交的python
+
 
     def before_build(self):
-        print("os:", platform.system())
         if platform.system() == 'Windows':
             # self.cmd('set PATH=%PYTHON%;%PYTHON%//scripts;%PATH%')
             # self.cmd('set VS90COMNTOOLS=%VS140COMNTOOLS%')
@@ -44,6 +45,9 @@ class PythonBuild(base.BuildBase):
                 "C:\\Python38\\python.exe",
                 "C:\\Python38-x64\\python.exe",
             ]
+
+            self.twinePython = "C:\\Python36-x64\\python.exe"
+
         elif platform.system() == "Linux":
             self.pythonList = [
                 "$HOME/venv3.9/bin/python",
@@ -55,6 +59,8 @@ class PythonBuild(base.BuildBase):
                 "$HOME/venv2.7/bin/python",
                 "$HOME/venv2.6/bin/python",
             ]
+
+            self.twinePython = "$HOME/venv3.6/bin/python"
         elif platform.system() == "Mac":
             self.pythonList = [
                 "$HOME/venv3.9/bin/python",
@@ -66,6 +72,8 @@ class PythonBuild(base.BuildBase):
                 "$HOME/venv2.7/bin/python",
                # "$HOME/venv2.6/bin/python",
             ]
+
+            self.twinePython = "$HOME/venv3.6/bin/python"
 
 
         # 拉取代码
@@ -82,3 +90,12 @@ class PythonBuild(base.BuildBase):
             self.cmd( python + "  -m pip install --upgrade setuptools")
             self.cmd( python + "  -m pip  install wheel")
             self.cmd( python + "  setup.py bdist_wheel")
+
+    
+    def after_build(self):
+        if platform.system() == 'Windows':
+            self.cmd("echo %PATH%")
+        else:
+            self.cmd("echo $PATH")
+        # self.cmd(self.twinePython + "  -m pip install twine")
+        # self.cmd(self.twinePython + "  -m twine upload -u yuangu -p $TWINE_PASS dist/*")
--- work.py
+++ work.py
@@ -1,24 +1,32 @@
+# import os
+# import sys
+
+# def p():
+#     frozen = "not"
+#     if getattr(sys, 'frozen',False):
+#         frozen = "ever so"
+#         return os.path.dirname(sys.executable)
+
+#     return os.path.split(os.path.realpath(__file__))[0]
+
+# pyPath = p()
+
+
+# sys.path.append( os.path.join(pyPath,  "./build"))
+
+# ## 如果是python构建任务，执行python构建任务
+# if os.getenv('BUILDFOR') == "python":
+#     from build import python
+#     python.PythonBuild().run()
+# else:
+#     print("不支持的构建语言目标")
+
+
+
+import platform
 import os
-import sys
 
-def p():
-    frozen = "not"
-    if getattr(sys, 'frozen',False):
-        frozen = "ever so"
-        return os.path.dirname(sys.executable)
-
-    return os.path.split(os.path.realpath(__file__))[0]
-
-pyPath = p()
-
-
-sys.path.append( os.path.join(pyPath,  "./build"))
-
-## 如果是python构建任务，执行python构建任务
-if os.getenv('BUILDFOR') == "python":
-    from build import python
-    python.PythonBuild().run()
+if platform.system() == 'Windows':
+    os.system("echo %PATH%")
 else:
-    print("不支持的构建语言目标")
-
-
+    os.system("echo $PATH")
